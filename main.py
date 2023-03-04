a958af0177924d4e8b27768bf787c87a4a9cff00
borysik <34368933+bvchirkov@users.noreply.github.com>
2023-03-04 12:50:38 +0400

    wip

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import BimDataModel
 from BimTools import Bim, Transit, Zone
 from BimComplexity import BimComplexity
 
-building = BimDataModel.mapping_building('resources/cfast-learn_2d.json')
+building = BimDataModel.mapping_building('resources/cfast-learn-2d.json')
 # building = BimDataModel.mapping_building('resources/building_example.json')
 
 bim = Bim(building)
