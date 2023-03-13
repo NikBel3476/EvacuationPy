cd94e20cdff982be16a852db6afadeeb4842fe8d
borysik <34368933+bvchirkov@users.noreply.github.com>
2023-03-13 20:58:00 +0400

    Added new exception type as UnknownException. It catches when zone is free from transits

--- BimDataModel.py
+++ BimDataModel.py
@@ -48,6 +48,7 @@ class BBuilding:
 
 def mapping_building(file_buildingjson:str) -> BBuilding:
     building:BBuilding
+    bad_elements = list()
 
     with open(file_buildingjson, 'r') as json_file:
         rjson = json.load(json_file)
@@ -59,17 +60,28 @@ def mapping_building(file_buildingjson:str) -> BBuilding:
                 _sign = BSign.from_str(element['Sign'])
                 _sizeZ = element['SizeZ'] if not (_sign == BSign.DoorWay) else 0.0
 
-                build_element = BBuildElement(id=UUID(element['Id']),
+                try:
+                    build_element = BBuildElement(id=UUID(element['Id']),
                                                     sign=_sign,
                                                     name=element['Name'],
                                                     sizeZ=_sizeZ,
                                                     output=[UUID(uuid) for uuid in element['Output']],
                                                     points=[BPoint(p['x'], p['y'], level['ZLevel']) for p in element['XY'][0]['points']])
-
-                _elements.append(build_element)
+                    _elements.append(build_element)
+                except:
+                    bad_elements.append((_sign, element['Name'], element['Id'], level['ZLevel']))
 
             _levels.append(BLevel(name=level['NameLevel'], zlevel=level['ZLevel'], elements=_elements))
 
         building = BBuilding(name=rjson['NameBuilding'], levels=_levels)
+    
+    if len(bad_elements) > 0:
+        import inspect
+        print(f">UnknownException[{__file__}:{inspect.currentframe().f_lineno}]. Please check elements from list bellow:")
+        for sign, name, id, level in bad_elements:
+            print(f"{sign.name}({id}), name={name}, level={level})")
+        print(">>QGIS expression for find bad elements (use 'Select Features Using Expression'):")
+        print(' or '.join(f'id is \'{id}\'' for _,_,id,_ in bad_elements))
+        exit()
     
     return building
