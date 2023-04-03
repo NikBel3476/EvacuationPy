db2f2f34c2a57e8895dd50ef19feb3a7a35c97d0
borysik <34368933+bvchirkov@users.noreply.github.com>
2023-04-03 15:00:12 +0400

    Fix

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import BimDataModel
 from BimTools import Bim
 from BimComplexity import BimComplexity
+from BimEvac import Moving
 
-building = BimDataModel.mapping_building('resources/cfast-learn-2d.json')
+building = BimDataModel.mapping_building('resources/example-one-exit.json')
 # building = BimDataModel.mapping_building('resources/building_example.json')
 
 bim = Bim(building)
 
-bc = BimComplexity(bim)
-print(bc)
+BimComplexity(bim) # check a building
+
+m = Moving()
+
+m.step(bim)
